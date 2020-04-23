3e88bdf569ec93d8ad03c2f37d40abab1a18bea1
kmj <moutype123@gmail.com>
2020-04-24 00:52:02 +0900

    backjoon N1931 solve

--- solve/Tree_N_11725.py
+++ solve/Tree_N_11725.py
@@ -1,13 +1,14 @@
-node_Number = int(input()); tree = {}
-check = {1:1}
-for i in range(1,node_Number):
-    key, value = map(int,input().split())
-    if value in check:
-        tree[key] = value
-        check[key] = key
-        continue
-    tree[value] = key
-
-for i in range(2,node_Number+1):
-    print(tree[i])
-
+import sys
+n = int(input());t = {i:[] for i in range(1,n+1) };s = [1];p = {}
+for _ in range(n-1):
+    a,b = map(int, sys.stdin.readline().split())
+    t[a].append(b)
+    t[b].append(a)
+while s:
+    a = s.pop(0)
+    for i in t[a]:
+        s.append(i)
+        p[i] = a
+        t[i].remove(a)
+for i in range(2,n+1):
+    print(p[i])
